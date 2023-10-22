c012ea7d18dbcf0ecbc2aabc70f6ea88b99aacd1
Borovskyi93 <85964928+Borovskyi93@users.noreply.github.com>
2023-10-22 20:52:11 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -13,14 +13,80 @@ console.log('Середнє арифметичне числових елемен
 
 /*------------------------------------------------2----------------------------------------------*/
 console.log('*-------------------------------------------2--------------------------------------*');
-
 const firstNumber = +prompt ('Введіть перше число:');
 const secondNumber = +prompt ('Введіть друге число:');
 const operationWithNumbers = prompt ('Введіть знак операції обчислення:');
 
-function doMath (firstNumber, secondNumber, operation) {
-    switch (firstNumber, secondNumber)
-        case 
+
+function doMath (firstNumber, secondNumber, operationWithNumbers) {
+    
+    if (firstNumber && secondNumber) {
+        switch(operationWithNumbers) {
+            case '+':
+                console.log(`${firstNumber} + ${secondNumber} = ${firstNumber + secondNumber}`);
+                break;
+            case '-':
+                console.log(`${firstNumber} - ${secondNumber} = ${firstNumber - secondNumber}`);
+                break;
+            case '*':
+                console.log(`${firstNumber} * ${secondNumber} = ${firstNumber * secondNumber}`);
+                break;
+            case '/':
+                console.log(`${firstNumber} / ${secondNumber} = ${firstNumber / secondNumber}`);
+                break;
+            case '%':
+                console.log(`${firstNumber} % ${secondNumber} = ${firstNumber % secondNumber}`);
+                break;
+            case '^':
+                console.log(`${firstNumber} ** ${secondNumber} = ${firstNumber ** secondNumber}`);
+                break;
+            default:
+                console.log('Невірні дані!');
+        }
+    } else {
+        console.log('Невірні дані!');
+    }
 }
 
 doMath(firstNumber, secondNumber, operationWithNumbers);
+
+/*------------------------------------------------3----------------------------------------------*/
+console.log('*-------------------------------------------3--------------------------------------*');
+
+const basicArrayLength = +prompt('Введіть довжину основного масиву:');
+const insideElementsArrayLength = +prompt('Введіть довжину внутрішнього масиву:');
+
+function toFillArrayElements (baseArrLength, insideArrLength) {
+    const baseArray = [];
+
+    for (let countBasicArr = 0; countBasicArr < baseArrLength; countBasicArr++) {
+        baseArray[countBasicArr] = [];
+        for (let insideCountArr = 0; insideCountArr < insideArrLength; insideCountArr++) {
+            let elementsFromUser = +prompt('Елементи масиву:');
+            baseArray[countBasicArr][insideCountArr] = elementsFromUser;
+        }
+    }
+    return baseArray;
+}
+
+console.log(toFillArrayElements(basicArrayLength, insideElementsArrayLength));
+
+/*------------------------------------------------4----------------------------------------------*/
+console.log('*-------------------------------------------4--------------------------------------*');
+
+function toDeleteChar (string, charToDelete) {
+    let stringToArray = string.split('');
+
+    for (let index of stringToArray) {
+        for (let indexIn of charToDelete) {
+            if (index === indexIn) {
+                stringToArray.splice(stringToArray.indexOf(index), 1);
+            }
+        }
+    }
+
+    let newString = stringToArray.join('');
+    return newString;
+}
+
+console.log(toDeleteChar('hello world', ['l', 'o']));
